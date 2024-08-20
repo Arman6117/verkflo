74f8e34d4f98488b2047a6797c2b8b6663e70e63
Arman Patel <vanixgaming634@gmail.com>
2024-08-20 12:12:22 +0530

    Initilised

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+     <div className="flex justify-center items-center ">
+      <h1>Seamlessly track and share project update </h1>
+     </div>
+    </>
+  );
+}
